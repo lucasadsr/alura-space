00e801b3e4bee1206131eeeb45d3bb24dc18673f
Lucas Araújo <lucasaraujodesa@hotmail.com>
2022-11-27 17:05:25 -0300

    adicionando componente Cabecalho

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './styles/estilosGlobais.scss'
+import PaginaInicial from "./pages/PaginaInicial/PaginaInicial";
+
+
+export default function App() {
+  return (
+    <PaginaInicial />
+  );
+}

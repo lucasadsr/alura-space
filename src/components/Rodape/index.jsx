65d32d5e56fe6764d1e763780621c1e8abf88ce6
Lucas Araújo <lucasaraujodesa@hotmail.com>
2022-12-06 22:35:41 -0300

    Adicionando Footer

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,35 @@
+import facebook from '../../assets/icones/fb.png'
+import twitter from '../../assets/icones/tw.png'
+import instagram from '../../assets/icones/ig.png'
+import styles from './Rodape.module.scss'
+
+export default function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <div className={styles.rodape__icones}>
+                <a
+                href="https://www.facebook.com/AluraCursosOnline/"
+                target="_blank"
+                rel="noreferrer"
+                >
+                <img src={facebook} alt="ícone do facebook" />
+                </a>
+                <a
+                href="https://twitter.com/aluraonline?lang=en"
+                target="_blank"
+                rel="noreferrer"
+                >
+                <img src={twitter} alt="ícone do twitter" />
+                </a>
+                <a
+                href="https://www.instagram.com/aluraonline/?hl=en"
+                target="_blank"
+                rel="noreferrer"
+                >
+                <img src={instagram} alt="ícone do instagram" />
+                </a>
+            </div>
+            <p>Desenvolvido por Alura</p>
+        </footer>
+    )
+}

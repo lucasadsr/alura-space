6f1ef931b1a32e60a7c5468233eec714f8b8ce66
Lucas Araújo <lucasaraujodesa@hotmail.com>
2022-12-09 00:00:43 -0300

    Adicionando funcionalidade de filtro por tags

--- src/pages/PaginaInicial/PaginaInicial.jsx
+++ src/pages/PaginaInicial/PaginaInicial.jsx
@@ -1,10 +1,11 @@
 import banner from './banner.png'
-import Cabecalho from "../../components/Cabecalho";
-import Menu from "../../components/Menu";
 import styles from './PaginaInicial.module.scss'
-import Rodape from '../../components/Rodape';
-import Galeria from '../../components/Galeria';
-import Populares from '../../components/Populares';
+
+import Cabecalho from "components/Cabecalho";
+import Menu from "components/Menu";
+import Rodape from 'components/Rodape';
+import Galeria from 'components/Galeria';
+import Populares from 'components/Populares';
 
 export default function PaginaInicial() {
     return (
